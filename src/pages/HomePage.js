668321e966206571f577dc58b6be6934ae77af3a
valerie9922 <valerie.zheng@gmail.com>
2022-09-20 13:20:54 +0200

    HomePage : added button; finished V1.1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,27 @@
 
-import React, { useState, useRef } from 'react'
-import { LuckyWheel } from '@lucky-canvas/react'
+import React, { useState, useRef } from 'react';
+import { LuckyWheel } from '@lucky-canvas/react';
+import {useDispatch} from "react-redux";
+import {selectCategory} from "../store/questionsPage/questionSelectors";
+import {categorySelected} from "../store/questionsPage/QuestionSlice";
+import {CATEGORY} from "../config/constants";
+import {Link} from "react-router-dom";
 
 const HomePage = () => {
         const [selected, setSelected]=useState('');
+        const dispatch=useDispatch();
+
     const [blocks] = useState([
         { padding: '10px', background: '#869cfa' }
     ])
-    const [prizes] = useState([
-        { background: '#e9e8fe', fonts: [{ text: 'Food' }] },
-        { background: '#b8c5f2', fonts: [{ text: 'Sports' }] },
-        { background: '#e9e8fe', fonts: [{ text: 'Science' }] },
-        { background: '#b8c5f2', fonts: [{ text: 'Places' }] },
 
+    const [prizes] = useState([
+        { background: '#e9e8fe', fonts: [{ text: CATEGORY[0] }] },
+        { background: '#b8c5f2', fonts: [{ text: CATEGORY[1]}] },
+        { background: '#e9e8fe', fonts: [{ text: CATEGORY[2]}] },
+        { background: '#b8c5f2', fonts: [{ text: CATEGORY[3]}] }
     ])
+
     const [buttons] = useState([
         { radius: '40%', background: '#617df2' },
         { radius: '35%', background: '#afc8ff' },
@@ -23,6 +31,7 @@ const HomePage = () => {
             fonts: [{ text: 'Start', top: '-10px' }]
         }
     ])
+
     const myLucky = useRef();
     return (
         <div align={"center"}>
@@ -39,21 +48,33 @@ const HomePage = () => {
                     setTimeout(() => {
                         const index = Math.random() * 4 >> 0
                         myLucky.current.stop(index)
-                    }, 2500)
+                    }, 2000)
                 }}
 
                 onEnd={prize => { // 抽奖结束会触发end回调
-                    setSelected(prize.fonts[0].text);
-                   // alert('You chosed ' + prize.fonts[0].text + ' Category');
+                    const category=prize.fonts[0].text;
+                    setSelected(category);
+                    dispatch(categorySelected(category));
+                   //alert('You chosed ' + prize.fonts[0].text + ' Category');
                 }}
             />
             <div sx={{display:"flex",flexDirection:"row"}}>
                <div><b>Categories:</b> </div> <div>Food</div>
                 <div>Sports</div>  <div>Science</div>  <div>Places</div>
             </div>
+
+            <div>
             {selected!==''?
             <h4>your choice is :{selected}  </h4>
-           :null }
+             :null }
+            </div>
+
+            <div>
+                <Link to='/questions/:id' style={{ textAlign: "center" }}>
+                     <button>Start Question!</button>
+                </Link>
+            </div>
+
         </div>
 
 
