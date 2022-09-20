6b87f60fdfca12da2f2eb29d4aad2e2e4b09ed66
navanjani <navanjani@outlook.com>
2022-09-20 15:00:00 +0200

    Add finalscreen page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GamePage from "./pages/GamePage";
+import FinalScreen from "./pages/FinalScreen";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/:categoryId/questions" element={<GamePage />} />
+        <Route path="/quiz" element={<GamePage />} />
+        <Route path="/final" element={<FinalScreen />} />
       </Routes>
     </div>
   );
