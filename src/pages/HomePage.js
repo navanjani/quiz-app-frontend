b3a6203e65a393564d4a973d993e7bdfae251d98
navanjani <navanjani@outlook.com>
2022-09-22 16:39:53 +0200

    Add home page style and more fixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import { selectCategoryArr } from "../store/questionsPage/questionSelectors";
 import { fetchCategoryArr } from "../store/questionsPage/QuestionActions";
 import { categorySelected } from "../store/questionsPage/QuestionSlice";
+import "./style.css";
+
 const HomePage = () => {
   const [selectedId, setSelectedId] = useState(0);
   const dispatch = useDispatch();
@@ -35,60 +37,48 @@ const HomePage = () => {
   ]);
 
   const myLucky = useRef();
+
   return (
-    <div align={"center"}>
-      <h2>Random start from a category: </h2>
-      {categoryList.length > 1 ? (
-        <LuckyWheel
-          ref={myLucky}
-          width="300px"
-          height="300px"
-          blocks={blocks}
-          prizes={prizes}
-          buttons={buttons}
-          onStart={() => {
-            // 点击抽奖按钮会触发star回调
-            myLucky.current.play();
-            setTimeout(() => {
-              const index = (Math.random() * 4) >> 0;
-              myLucky.current.stop(index);
-            }, 2000);
-          }}
-          onEnd={(prize) => {
-            // 抽奖结束会触发end回调
-            console.log(categoryList);
-            const category = prize.fonts[0].text;
-            setSelectedId(categoryList.indexOf(category) + 1);
-            dispatch(categorySelected(category));
-          }}
-        />
-      ) : (
-        <div>Something wrong!</div>
-      )}
+    <div className="home-container">
+      <div align={"center"}>
+        <h2>Random start from a category: </h2>
+        {categoryList.length > 1 ? (
+          <LuckyWheel
+            ref={myLucky}
+            width="500px"
+            height="500px"
+            blocks={blocks}
+            prizes={prizes}
+            buttons={buttons}
+            onStart={() => {
+              // 点击抽奖按钮会触发star回调
+              myLucky.current.play();
+              setTimeout(() => {
+                const index = (Math.random() * 4) >> 0;
+                myLucky.current.stop(index);
+              }, 2000);
+            }}
+            onEnd={(prize) => {
+              // 抽奖结束会触发end回调
+              console.log(categoryList);
+              const category = prize.fonts[0].text;
+              setSelectedId(categoryList.indexOf(category) + 1);
+              dispatch(categorySelected(category));
+            }}
+          />
+        ) : (
+          <div>Something wrong!</div>
+        )}
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          margin: 30,
-          backgroundColor: "aliceblue",
-        }}
-      >
-        <div>Categories: </div> <div>Food</div>
-        <div>Sports</div> <div>Science</div> <div>Places</div>
-      </div>
+        {/*<div>*/}
+        {/*  {selectedId !== 0 ? <h4> {CATEGORY[selectedId - 1]} </h4> : null}*/}
+        {/*</div>*/}
 
-      <div>
-        {selectedId !== 0 ? (
-          <h4>your choice is :{CATEGORY[selectedId - 1]} </h4>
-        ) : null}
-      </div>
-
-      <div>
-        <Link to={`/questions/${selectedId}`} style={{ textAlign: "center" }}>
-          <button>Start Question!</button>
-        </Link>
+        <div className="start-question-wrapper">
+          <Link to={`/questions/${selectedId}`} style={{ textAlign: "center" }}>
+            <button>Start Question!</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
